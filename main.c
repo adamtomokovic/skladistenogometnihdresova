aedadd6d5d7e81ed6ccc38480edb21b41f1a2aa0
adamtomokovic <tomokovica@gmail.com>
2026-05-26 17:12:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "menu.h"
+#include "dresovi.h"
 
+int main()
+{
+    int opcija;
+
+    do
+    {
+        prikaziMenu();
+
+        printf("Odaberi opciju: ");
+        scanf("%d", &opcija);
+
+        switch (opcija)
+        {
+        case 1:
+            dodajDres();
+            break;
+
+        case 2:
+            prikaziDresove();
+            break;
+
+        case 3:
+            urediDres();
+            break;
+
+        case 4:
+            obrisiDres();
+            break;
+
+        case 0:
+            printf("Izlaz iz programa.\n");
+            break;
+
+        default:
+            printf("Neispravna opcija!\n");
+        }
+
+    } while (opcija != 0);
+
+    return 0;
+}
