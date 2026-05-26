e2583a36d2f76f3390cd8abc56976c3cbf399e95
adamtomokovic <tomokovica@gmail.com>
2026-05-26 18:09:53 +0200

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,7 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #ifndef MENU_H
 #define MENU_H
+typedef enum {
+	IZLAZ = 0,
+	DODAJ,      //enum 11.
+	PRIKAZI,    
+	UREDI,      
+	OBRISI      
+} OpcijaIzbornika;
 
-void prikaziMenu();
+void PrikaziMenu();
 
 #endif
