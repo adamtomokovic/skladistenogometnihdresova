365bd5a1205a2be0fa560b71b9da999f556d7db8
adamtomokovic <tomokovica@gmail.com>
2026-05-26 18:09:20 +0200

    Update dresovi.h

--- dresovi.h
+++ dresovi.h
@@ -3,36 +3,32 @@
 
 #define MAX_DRESOVA 100
 
-
-typedef enum
+typedef enum //enum
 {
-    XS,
-    S,
-    M,
-    L,
-    XL
+	XS, S, M, L, XL
 } Velicina;
-
 
 typedef struct
 {
-    int id;
-    char klub[50];
-    char igrac[50];
-    int broj;
-    Velicina velicina;
-    float cijena;
-    int kolicina;
+	int id;
+	char klub[50];
+	char igrac[50];
+	int broj;
+	Velicina velicina;
+	float cijena;
+	int kolicina;
 } Dres;
 
+extern int brojacOperacija;
 
-extern Dres dresovi[MAX_DRESOVA];
-extern int brojDresova;
+// 9 makro funkcije
+#define UKUPNA_VRIJEDNOST(c, k) ((c) * (k))
+#define JE_PRAZNO(b) ((b) == 0) 
 
-void dodajDres();
-void prikaziDresove();
-void urediDres();
-void obrisiDres();
-void prikaziVelicinu(Velicina velicina);
+void DodajDres();
+void PrikaziDresove();
+void UrediDres();
+void ObrisiDres();
+void PrikaziVelicinu(const Dres* dresPtr);
 
 #endif
