9004a9db1cdcfd5c6af9001d7b381353375120a8
adamtomokovic <tomokovica@gmail.com>
2026-05-26 17:13:07 +0200

    Create menu.c

---
+++ menu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "menu.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+
+void prikaziMenu()
+{
+    printf("\n=============================\n");
+    printf(" SKLADISTE NOGOMETNIH DRESOVA\n");
+    printf("=============================\n");
+
+    printf("1. Dodaj dres\n");
+    printf("2. Prikazi dresove\n");
+    printf("3. Uredi dres\n");
+    printf("4. Obrisi dres\n");
+    printf("0. Izlaz\n");
+
+    printf("=============================\n");
+}
